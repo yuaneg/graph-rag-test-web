f4ee0b6146ceb5427b69f495f140d711a47ab12b
yuaneg <yuanenguang@300.cn>
2024-10-11 16:36:33 +0800

    提示词优化

--- web/train.py
+++ web/train.py
@@ -1,15 +1,6 @@
 from graphrag.index.cli import index_cli
 from graphrag.logging import ReporterType
-from graphrag.query.cli import reporter
 from graphrag.index.emit.types import TableEmitterType
-
-# rm -rf $1
-# mkdir -p ./$1/input
-# cp $2 ./$1/input
-# python -m graphrag.index --init --root ./$1
-# rm -rf ./$1/settings.yaml
-# cp  -rf ./config/settings.yaml ./$1/
-# python -m graphrag.index --root ./$1
 
 if __name__ == '__main__':
     index_cli(
